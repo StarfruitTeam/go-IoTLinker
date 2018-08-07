95955df562eedd81c1f51d51736e70400b8ebc2d
ParkJunYong <pjy1418@gmail.com>
2018-08-07 23:34:27 +0900

    add: mqtt client sample

--- net/mqtt_client/main.go
+++ net/mqtt_client/main.go
@@ -8,59 +8,100 @@ package main
 	 "os"
  )
 
-var f MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
-	fmt.Printf("TOPIC:%s\n",msg.Topic())
-	fmt.Printf("TOPIC:%s\n",msg.MessageID())
-}
+/*
+Options:
+ [-help]                      Display help
+ [-a pub|sub]                 Action pub (publish) or sub (subscribe)
+ [-m <message>]               Payload to send
+ [-n <number>]                Number of messages to send or receive
+ [-q 0|1|2]                   Quality of Service
+ [-clean]                     CleanSession (true if -clean is present)
+ [-id <clientid>]             CliendID
+ [-user <user>]               User
+ [-password <password>]       Password
+ [-broker <uri>]              Broker URI
+ [-topic <topic>]             Topic
+ [-store <path>]              Store Directory
+*/
 
+	
 func main(){
-	//topic := flag.String("topic", "", "The topic name to/from which to publish/subscribe")
+	topic := flag.String("topic", "", "The topic name to/from which to publish/subscribe")
 	broker := flag.String("broker", "tcp://127.0.0.1:1883", "The broker URI. ex: tcp://10.10.1.1:1883")
-	//password := flag.String("password", "", "The password (optional)")
-	//user := flag.String("user", "", "The User (optional)")
-	//id := flag.String("id", "testgoid", "The ClientID (optional)")
-	//cleansess := flag.Bool("clean", false, "Set Clean Session (default false)")
-	//qos := flag.Int("qos", 0, "The Quality of Service 0,1,2 (default 0)")
-	//num := flag.Int("num", 1, "The number of messages to publish or subscribe (default 1)")
-	//payload := flag.String("message", "", "The message text to publish (default empty)")
-	//action := flag.String("action", "", "Action publish or subscribe (required)")
-	//store := flag.String("store", ":memory:", "The Store Directory (default use memory store)")
+	password := flag.String("password", "", "The password (optional)")
+	user := flag.String("user", "", "The User (optional)")
+	id := flag.String("id", "testgoid", "The ClientID (optional)")
+	cleansess := flag.Bool("clean", false, "Set Clean Session (default false)")
+	qos := flag.Int("qos", 0, "The Quality of Service 0,1,2 (default 0)")
+	num := flag.Int("num", 1, "The number of messages to publish or subscribe (default 1)")
+	payload := flag.String("message", "", "The message text to publish (default empty)")
+	action := flag.String("action", "", "Action publish or subscribe (required)")
+	store := flag.String("store", ":memory:", "The Store Directory (default use memory store)")
+	
 	flag.Parse()
 
-	opts := MQTT.NewClientOptions().AddBroker(*broker)
-
-	opts.SetKeepAlive(2 * time.Second)
-	opts.SetDefaultPublishHandler(f)
-	opts.SetPingTimeout(1 * time.Second)
-
-
-	client := MQTT.NewClient(opts)
-
-	if token := client.Connect();token.Wait() && token.Error() != nil {
-	panic(token.Error())
+	// 각 필수 항목 검사
+	if *action != "pub" && *action != "sub" {
+		fmt.Println("Invalid setting for -action, must be pub or sub")
+		return
 	}
 
-	if token := client.Subscribe("go-mqtt/sample", 0, nil); token.Wait() && token.Error() != nil {
-		fmt.Println(token.Error())
-		os.Exit(1)
+	if *topic == "" {
+		fmt.Println("Invalid setting for -topic, most not be empty")
+		return
 	}
 
-	for i := 0; i < 5; i++ {
-		text := fmt.Sprintf("this is msg #%d!", i)
-		token := client.Publish("go-mqtt/sample", 0, false, text)
-		token.Wait()
+	opts := MQTT.NewClientOptions()
+	opts.AddBroker(*broker)
+	opts.SetClientID(*id)
+	opts.SetUsername(*user)
+	opts.SetPassword(*password)
+	opts.SetCleanSession(*cleansess)
+	
+	if *store != ":memory:" {
+		opts.SetStore(MQTT.NewFileStore(*store))
 	}
 
-	time.Sleep(6 * time.Second)
+	if *action == "pub" {
+		client := MQTT.NewClient(opts)
+		if token := client.Connect(); token.Wait() && token.Error() != nil {
+			panic(token.Error())
+		}
+		fmt.Println("Sample Publisher Started")
+		for i := 0; i < *num; i++ {
+			fmt.Println("---- doing publish ----")
+			token := client.Publish(*topic, byte(*qos), false, *payload)
+			token.Wait()
+		}
 
-	if token := client.Unsubscribe("go-mqtt/sample"); token.Wait() && token.Error() != nil {
-		fmt.Println(token.Error())
-		os.Exit(1)
-	}
+		client.Disconnect(250)
+		fmt.Println("Sample Publisher Disconnected")
+	} else {
+		receiveCount := 0
+		choke := make(chan [2]string)
 
-	client.Disconnect(250)
+		opts.SetDefaultPublishHandler(func(client MQTT.Client, msg MQTT.Message) {
+			choke <- [2]string{msg.Topic(), string(msg.Payload())}
+		})
 
-	time.Sleep(1 * time.Second)
+		client := MQTT.NewClient(opts)
+		if token := client.Connect(); token.Wait() && token.Error() != nil {
+			panic(token.Error())
+		}
+
+		if token := client.Subscribe(*topic, byte(*qos), nil); token.Wait() && token.Error() != nil {
+			fmt.Println(token.Error())
+			os.Exit(1)
+		}
+
+		for receiveCount < *num {
+			incoming := <-choke
+			fmt.Printf("RECEIVED TOPIC: %s MESSAGE: %s\n", incoming[0], incoming[1])
+			receiveCount++
+		}
+
+		client.Disconnect(250)
+
 }
 
 
