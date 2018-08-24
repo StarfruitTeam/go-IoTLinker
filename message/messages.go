9273df065cc6d8ba0dd26c168d611b510df42abf
ParkJunYong <pjy1418@gmail.com>
2018-08-24 11:20:44 +0900

    add: models

--- message/messages.go
+++ message/messages.go
@@ -3,7 +3,13 @@ package nats
 
 //nats 사용되는 메시지 인터페이스
 
+type Tag interface {
+	getName() string
+}
+
+
 type message interface {
 	MsgTopic() string
-	MsgContent() string
+	MsgData() string
+	Tags() []string
 }
---
+++ models/models.go
@@ -0,0 +1,28 @@
+
+//Example
+type Person struct {
+        Name string
+        Phone string
+}
+
+type Tag struct {
+	name string
+	topic string
+	tag_type string
+	subscription integer
+	data_Type string
+	created datetime
+}
+
+type Device struct {
+	uuid string
+	name string
+	protocol string
+	tags []string
+	location string
+	timeout
+	subscription integer
+	data_type string
+	created datetime
+}
+
