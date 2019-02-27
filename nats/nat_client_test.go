6ccbf5baf8fada8a2e535ac5cee875908c3edee9
ParkJunYong <pjy1418@gmail.com>
2019-02-27 23:14:07 +0900

    - edit main.go - add nat_client_test.go - edit mqtt_client.go - add mqtt_client_test.go - edit receiver.go - edit service_test.go

---
+++ nats/nat_client_test.go
@@ -0,0 +1,17 @@
+package nats
+
+import ("testing"
+	"nats_client.go")
+
+func nats_client_connect(t testing.T){
+
+
+}
+
+func nats_client_Publish(t testing.T){
+	
+}
+
+
+func nats_client_Subscribe(t tesging.T){
+}
